c2ee8bff6dc75e65a55323415dd2686ab03d638c
Edgar Fuste <efuste@bgeo.es>
2022-11-11 13:27:20 +0100

    Enhancement: improve priority manager form. populate table view and manage triggers

--- core/toolbars/assetmanage/priority.py
+++ core/toolbars/assetmanage/priority.py
@@ -9,10 +9,11 @@ from datetime import datetime
 from functools import partial
 import os
 
-from qgis.PyQt.QtWidgets import QMenu, QAction, QActionGroup
+from qgis.PyQt.QtWidgets import QMenu, QAction, QActionGroup, QTableView
 from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtSql import QSqlTableModel
 
-from ....settings import tools_qgis, tools_qt, tools_gw, dialog, tools_os, tools_log, tools_db
+from ....settings import tools_qgis, tools_qt, tools_gw, dialog, tools_os, tools_log, tools_db, gw_global_vars
 from .... import global_vars
 
 from ...ui.ui_manager import PriorityUi, PriorityManagerUi
@@ -100,6 +101,10 @@ class AmPriority(dialog.GwAction):
 
         self.dlg_priority_manager = PriorityManagerUi()
 
+
+        self._fill_table(self.dlg_priority_manager, self.dlg_priority_manager.tbl_selection, "asset.result_selection",
+                         set_edit_triggers=QTableView.DoubleClicked)
+        tools_gw.set_tablemodel_config(self.dlg_priority_manager, self.dlg_priority_manager.tbl_selection, "result_selection", schema_name='asset')
 
         # Open the dialog
         tools_gw.open_dialog(self.dlg_priority_manager, dlg_name='priority')
@@ -200,3 +205,40 @@ class AmPriority(dialog.GwAction):
 
 
     # endregion
+
+    def _fill_table(self, dialog, widget, table_name, hidde=False, set_edit_triggers=QTableView.NoEditTriggers, expr=None):
+        """ Set a model with selected filter.
+            Attach that model to selected table
+            @setEditStrategy:
+            0: OnFieldChange
+            1: OnRowChange
+            2: OnManualSubmit
+        """
+        try:
+
+            # Set model
+            model = QSqlTableModel(db=gw_global_vars.qgis_db_credentials)
+            model.setTable(table_name)
+            model.setEditStrategy(QSqlTableModel.OnFieldChange)
+            model.setSort(0, 0)
+            model.select()
+
+            # When change some field we need to refresh Qtableview and filter by psector_id
+            # model.dataChanged.connect(partial(self._refresh_table, dialog, widget))
+            widget.setEditTriggers(set_edit_triggers)
+
+            # Check for errors
+            if model.lastError().isValid():
+                print(f"ERROR -> {model.lastError().text()}")
+
+            # Attach model to table view
+            if expr:
+                widget.setModel(model)
+                widget.model().setFilter(expr)
+            else:
+                widget.setModel(model)
+
+            if hidde:
+                self.refresh_table(dialog, widget)
+        except Exception as e:
+            print(f"EXCEPTION -> {e}")
