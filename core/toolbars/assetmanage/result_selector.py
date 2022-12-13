4e410bc3e7b36432c2b5fc56d45c0a4254b8e0b3
Pablo Marques <pahbloo@gmail.com>
2022-12-13 10:14:59 -0300

    Set combos of result selector

--- core/toolbars/assetmanage/result_selector.py
+++ core/toolbars/assetmanage/result_selector.py
@@ -7,15 +7,13 @@ or (at your option) any later version.
 # -*- coding: utf-8 -*-
 from qgis.PyQt.QtWidgets import QMenu, QAction, QActionGroup, QTableView
 
-from ....settings import tools_qgis, tools_qt, tools_gw, dialog, tools_os, tools_log, tools_db, gw_global_vars
+from ....settings import tools_qt, tools_gw, dialog, tools_db
 from .... import global_vars
 
 from ...ui.ui_manager import ResultSelectorUi
 
 
 class ResultSelector(dialog.GwAction):
-    """  """
-
     def __init__(self, icon_path, action_name, text, toolbar, action_group):
 
         super().__init__(icon_path, action_name, text, toolbar, action_group)
@@ -27,23 +25,48 @@ class ResultSelector(dialog.GwAction):
         self.toolbar = toolbar
         self.action_group = action_group
 
-
     def clicked_event(self):
         self.open_manager()
-
 
     def open_manager(self):
 
         self.dlg_result_selector = ResultSelectorUi()
+        dlg = self.dlg_result_selector
 
-        # Combo result_selector
-        sql = "SELECT cr.result_id as id, cr.result_name as idval FROM asset.selector_result_main sm join asset.cat_result cr using (result_id);"
-        rows = tools_db.get_rows(sql)
-        tools_qt.fill_combo_values(self.dlg_result_selector.cmb_result_selector, rows, 1, sort_by=0)
-        # Combo result_global
-        sql = "SELECT cr.result_id as id, cr.result_name as idval FROM asset.selector_result_compare sc join asset.cat_result cr using (result_id);"
-        rows = tools_db.get_rows(sql)
-        tools_qt.fill_combo_values(self.dlg_result_selector.cmb_result_global, rows, 1, sort_by=0)
+        results = tools_db.get_rows(
+            """
+            select result_id id, result_name idval
+            from asset.cat_result
+            """
+        )
+
+        # Combo result_main
+        tools_qt.fill_combo_values(dlg.cmb_result_main, results, 1, sort_by=1)
+        selected_main = tools_db.get_row(
+            """
+            select result_id
+            from asset.selector_result_main
+            where cur_user = current_user
+            """
+        )
+        if selected_main:
+            tools_qt.set_combo_value(
+                dlg.cmb_result_main, str(selected_main[0]), 0, add_new=False
+            )
+
+        # Combo result_compare
+        tools_qt.fill_combo_values(dlg.cmb_result_compare, results, 1, sort_by=1)
+        selected_compare = tools_db.get_row(
+            """
+            select result_id
+            from asset.selector_result_compare
+            where cur_user = current_user
+            """
+        )
+        if selected_compare:
+            tools_qt.set_combo_value(
+                dlg.cmb_result_compare, str(selected_compare[0]), 0, add_new=False
+            )
 
         # Open the dialog
-        tools_gw.open_dialog(self.dlg_result_selector, dlg_name='result_selection')
+        tools_gw.open_dialog(dlg, dlg_name="result_selection")
