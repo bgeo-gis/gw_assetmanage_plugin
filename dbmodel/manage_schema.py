1068a8798351255b34439aa8b1f6448f193214e2
Pablo Marques <pahbloo@gmail.com>
2023-01-06 11:35:32 -0300

    Run .sql files to create schema

--- dbmodel/manage_schema.py
+++ dbmodel/manage_schema.py
@@ -1,4 +1,6 @@
 import psycopg2
+from inspect import getsourcefile
+from pathlib import Path
 
 # Connection parameters
 DBNAME = None
@@ -22,3 +24,29 @@ if (
 ):
     print("There are some constants that have not been defined.")
     exit()
+
+files = ["ddl.sql", "tablect.sql"]
+sql_folder = Path(getsourcefile(lambda: 0)).parent
+
+conn = psycopg2.connect(
+    dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT
+)
+cur = conn.cursor()
+
+try:
+    for file in files:
+        with open(sql_folder / file) as f:
+            sql = (
+                f.read()
+                .replace("PARENT_SCHEMA", PARENT_SCHEMA)
+                .replace("SCHEMA_SRID", SCHEMA_SRID)
+            )
+        cur.execute(sql)
+    conn.commit()
+    print("Success!")
+except Exception as e:
+    conn.rollback()
+    print(e)
+finally:
+    cur.close()
+    conn.close()
