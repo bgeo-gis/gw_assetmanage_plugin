58eed5be623c2e57e83b887827aab6027c575f2f
Pablo Marques <pahbloo@gmail.com>
2023-01-23 10:48:59 -0300

    Fix previous commit for empty string case

--- core/toolbars/assetmanage/priority.py
+++ core/toolbars/assetmanage/priority.py
@@ -677,7 +677,8 @@ class CalculatePriority:
 
         exploitation = tools_qt.get_combo_value(dlg, "cmb_expl_selection") or None
         presszone = tools_qt.get_combo_value(dlg, "cmb_presszone") or None
-        diameter = f'{tools_qt.get_combo_value(dlg, "cmb_dnom"):g}' or None
+        diameter = tools_qt.get_combo_value(dlg, "cmb_dnom") or None
+        diameter = f'{diameter:g}' if diameter else None
         material = tools_qt.get_combo_value(dlg, "cmb_material") or None
 
         config_diameter = {}
