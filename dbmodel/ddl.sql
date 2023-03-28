88d240f9b1d0d904bc8b0909f7fa30352b81f91d
Edgar Fuste <efuste@bgeo.es>
2023-03-28 09:05:03 +0200

    Enhancement: add GNU headers

--- dbmodel/ddl.sql
+++ dbmodel/ddl.sql
@@ -1,3 +1,10 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
 CREATE SCHEMA asset;
 
 SET search_path = asset, public;
--- dbmodel/sample.sql
+++ dbmodel/sample.sql
@@ -1,3 +1,10 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
 --
 -- This file provides sample data for testing the gw_assetmanage_plugin with the
 -- Giswater sample water network.
