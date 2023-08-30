777890c6b9e668df665ce15a16304efa24019cdc
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2023-08-30 10:23:51 +0200

    Update headers files
    (cherry picked from commit 8e2f60bceb8d7ed1eb37d4c6cfcc1a03d2204c48)

--- core/ui/__init__.py
+++ core/ui/__init__.py
@@ -1,13 +1,10 @@
 """
-This file is part of Giswater 3
+Copyright © 2023 by BGEO. All rights reserved.
 The program is free software: you can redistribute it and/or modify it under the terms of the GNU
 General Public License as published by the Free Software Foundation, either version 3 of the License,
 or (at your option) any later version.
 """
 # -*- coding: utf-8 -*-
-__author__ = 'David Erill'
-__date__ = 'January 2016'
-__copyright__ = '(C) 2016, David Erill'
 
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
--- dbmodel/updates/2023-05/ddl.sql
+++ dbmodel/updates/2023-05/ddl.sql
@@ -1,3 +1,10 @@
+/*
+Copyright © 2023 by BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
 ALTER TABLE asset.config_catalog_def ADD COLUMN id SERIAL PRIMARY KEY;
 ALTER TABLE asset.value_result_type ADD PRIMARY KEY (id);
 ALTER TABLE asset.value_status ADD PRIMARY KEY (id);
