2dde09f4a88858b91b93d300cbe4927d4fa5854e
Pablo Marques <pahbloo@gmail.com>
2024-01-30 15:17:40 -0300

    Create corporate results view

--- dbmodel/updates/2024-01/ddl.sql
+++ dbmodel/updates/2024-01/ddl.sql
@@ -5,3 +5,40 @@ This version of Giswater is provided by Giswater Association
 */
 
 ALTER TABLE asset.cat_result ADD COLUMN iscorporate BOOLEAN;
+
+CREATE OR REPLACE VIEW asset.v_asset_arc_corporate
+AS SELECT o.arc_id,
+    o.result_id,
+    o.sector_id,
+    o.macrosector_id,
+    o.presszone_id,
+    o.builtdate,
+    o.arccat_id,
+    o.dnom,
+    o.matcat_id,
+    o.pavcat_id,
+    o.function_type,
+    o.the_geom,
+    o.code,
+    o.expl_id,
+    o.dma_id,
+    o.press1,
+    o.press2,
+    o.flow_avg,
+    o.longevity,
+    o.rleak,
+    o.nrw,
+    o.strategic,
+    o.mandatory,
+    o.compliance,
+    o.val,
+    o.orderby,
+    o.expected_year,
+    o.replacement_year,
+    o.budget,
+    o.total,
+    o.length,
+    o.cum_length
+   FROM asset.arc_output o
+     JOIN asset.cat_result r ON r.result_id = o.result_id
+  WHERE r.iscorporate = TRUE;
